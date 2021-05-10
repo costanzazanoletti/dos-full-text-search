ed85d185b79c0507289f4b69b70922bd574dfe57
costanzazanoletti <zanoletti.cos@gmail.com>
2021-05-10 10:53:27 +0200

    New query string

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
-
 const knex = require('knex')({
   client: 'pg',
   connection: process.env.DATABASE_URL,
@@ -10,38 +9,73 @@ const knex = require('knex')({
   },
 });
 
-const searchQuery =
-  'select ad.postcode, o.org_name, b.borough, b.clients, s.service, cc.cat_concat, s.document_vectors, ' +
-  'ts_rank_cd(s.document_vectors, query) as rank ' +
-  'from "Service" s ' +
-  'join "service_cat_concat_view" cc on cc.service_id = s.id ' +
-  'join "Branch" b on s.branch_id = b.id ' +
-  'left outer join "Address" ad on b.id = ad.branch_id ' +
-  'join "Organisation" o on b.org_id = o.id, ' +
-  'plainto_tsquery(:term) query ' +
-  'WHERE s.document_vectors @@ plainto_tsquery(:term) ' +
-  'order by rank desc;';
-const searchQueryLocation =
-  'select ad.postcode, o.org_name, b.borough, b.clients, ' +
-  's.service, cc.cat_concat, s.document_vectors, ' +
-  'ts_rank_cd(s.document_vectors, query) as rank, ' +
-  'case when loc.geom is null then 999999999999999 ' +
-  'else ST_DistanceSphere(loc.geom, pc.location) ' +
-  'end as distance ' +
-  'from postcode pc, ' +
-  '"Service" s ' +
-  'join service_cat_concat_view cc on cc.service_id = s.id ' +
-  'join "Branch" b on s.branch_id = b.id ' +
-  'left outer join "Address" ad on b.id = ad.branch_id ' +
-  'left outer join "Location" loc on ad.id = loc.address_id ' +
-  'join "Organisation" o on b.org_id = o.id, ' +
-  'plainto_tsquery(:term) query ' +
-  'WHERE s.document_vectors @@ plainto_tsquery(:term) ' +
-  'and pc.postcode in (:postcode) ' +
-  'order by distance, rank desc;';
+const searchQuery = `
+select 
+o.org_name, 
+adr.area, 
+b.borough, 
+b.project, 
+b.clients, 
+s.service,
+s.process,
+s.service_days,
+adr.telephone,
+adr.email_address,
+o.website,
+adr.postcode,
+b.tag,
+c.cat_concat, s.document_vectors,
+ts_rank_cd(s.document_vectors, query) as rank
+from "Service" s
+join "service_cat_concat_view" c on c.service_id = s.id
+join "Branch" b on s.branch_id = b.id
+join "Organisation" o on b.org_id = o.id
+join "Address" adr on adr.branch_id = b.id,
+plainto_tsquery(:term) query
+WHERE s.document_vectors @@ plainto_tsquery(:term)
+order by rank desc;
+`;
+
+const searchQueryLocation = `
+select
+o.org_name, 
+adr.area, 
+b.borough, 
+b.project, 
+b.clients, 
+s.service,
+s.process,
+s.service_days,
+adr.telephone,
+adr.email_address,
+o.website,
+adr.postcode,
+b.tag,
+c.cat_concat, 
+s.document_vectors,
+ts_rank_cd(s.document_vectors, query) as rank,
+case when loc.geom is null then 999999999999999
+else ST_DistanceSphere(loc.geom, pc.location)
+end as distance
+from postcode pc,
+"Service" s
+join service_cat_concat_view c on c.service_id = s.id
+join "Branch" b on s.branch_id = b.id
+left outer join "Address" adr on b.id = adr.branch_id
+left outer join "Location" loc on adr.id = loc.address_id 
+join "Organisation" o on b.org_id = o.id, 
+plainto_tsquery(:term) query
+WHERE s.document_vectors @@ plainto_tsquery(:term)
+and pc.postcode = :postcode
+order by distance, rank desc;
+`;
 
 module.exports = function (app) {
   app.use(bodyParser.json());
+
+  app.get('/api', function (req, res) {
+    res.json('this is the api service');
+  });
 
   app.post('/api/search', function (req, res) {
     const term = req.body.searchstring;
@@ -49,10 +83,12 @@ module.exports = function (app) {
     const params = { term: term, postcode: postcode };
     const queryStr =
       postcode && postcode !== '' ? searchQueryLocation : searchQuery;
-    // console.log(queryStr);
+    console.log(queryStr);
     knex
       .raw(queryStr, params)
-      .then((result) => res.json(result.rows))
+      .then((result) => {
+        res.json(result.rows);
+      })
       .catch((err) => {
         console.log(err);
         throw err;
